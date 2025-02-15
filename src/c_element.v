02894c6e4132eb71858f1046f88a4521586edb2f
Tórur Biskopstø Strøm <torur.strom@gmail.com>
2025-02-15 19:24:04 +0100

    do not instantiate cells in TEST

--- src/c_element.v
+++ src/c_element.v
@@ -11,13 +11,14 @@ input b;
 
 output y /* synthesis keep */;
 
-
-// Should correspond to
-// assign y = (a & b) | (y & (a | b));
-wire andabw, orabw, andyabw;
-sky130_fd_sc_hd__and2 andab(a, b, andabw);
-sky130_fd_sc_hd__or2 orab(a, b, orabw);
-sky130_fd_sc_hd__and2 andyab(y, orabw, andyabw);
-sky130_fd_sc_hd__or2 orabyab(andabw, andyabw, y);
+`ifdef TEST
+  assign y = (a & b) | (y & (a | b));
+`else
+  wire andabw, orabw, andyabw;
+  sky130_fd_sc_hd__and2 andab(a, b, andabw);
+  sky130_fd_sc_hd__or2 orab(a, b, orabw);
+  sky130_fd_sc_hd__and2 andyab(y, orabw, andyabw);
+  sky130_fd_sc_hd__or2 orabyab(andabw, andyabw, y);
+`endif
 
 endmodule
